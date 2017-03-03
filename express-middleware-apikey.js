018f4779e11800f368eb015004494976e2522553
Hidde Stokvis <hidde@neverhide.nl>
2017-03-03 14:47:04 +0100

    force full key

--- express-middleware-apikey.js
+++ express-middleware-apikey.js
@@ -2,7 +2,7 @@
 
 module.exports = function(api_keys) {
   return function(req, res, next) {
-      if (api_keys && api_keys.length != 0 && api_keys.indexOf(req.headers['x-api-key']) == -1) {
+      if (api_keys && api_keys.length != 0 && api_keys.indexOf(req.headers['x-api-key']) == -1 && api_keys[api_keys.indexOf(req.headers['x-api-key'])] !== req.headers['x-api-key']) {
           res.status(401).send({ error: 'Invalid api_key' });
       } else {
         delete req.headers['x-api-key'];
