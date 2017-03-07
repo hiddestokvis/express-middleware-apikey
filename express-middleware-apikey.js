89f86251a7c1ced80c52cae28f6315e69f30c2e8
Hidde Stokvis <hidde@neverhide.nl>
2017-03-07 16:11:59 +0100

    fix: test again?

--- express-middleware-apikey.js
+++ express-middleware-apikey.js
@@ -2,7 +2,7 @@
 
 module.exports = function(api_key) {
   return function(req, res, next) {
-      if (req.headers['x-api-key'] !== api_key) {
+      if (!req.headers['x-api-key'] || req.headers['x-api-key'] !== api_key) {
           res.status(401).send({ error: 'Invalid api_key' });
           return next({ error: 'Invalid api_key' });
       } else {
