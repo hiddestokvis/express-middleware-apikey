eae88881edbe7672464e04ae4e324b82e2e78b0d
Hidde Stokvis <hidde@neverhide.nl>
2017-03-07 16:04:53 +0100

    fix: no array

--- express-middleware-apikey.js
+++ express-middleware-apikey.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function(api_keys) {
+module.exports = function(api_key) {
   return function(req, res, next) {
-      if (api_keys && api_keys.indexOf(req.headers['x-api-key']) == -1 && api_keys[api_keys.indexOf(req.headers['x-api-key'])] !== req.headers['x-api-key'] || api_keys.length === 0) {
+      if (api_key !== req.headers['x-api-key'] || api_keys.length === 0) {
           res.status(401).send({ error: 'Invalid api_key' });
       } else {
         delete req.headers['x-api-key'];
