116c111ff5512561152b2ff61d483cd4be9cdeb0
Hidde Stokvis <hidde@neverhide.nl>
2017-03-07 15:59:54 +0100

    fix: don't allow empty api key

--- express-middleware-apikey.js
+++ express-middleware-apikey.js
@@ -2,7 +2,7 @@
 
 module.exports = function(api_keys) {
   return function(req, res, next) {
-      if (api_keys && api_keys.length != 0 && api_keys.indexOf(req.headers['x-api-key']) == -1 && api_keys[api_keys.indexOf(req.headers['x-api-key'])] !== req.headers['x-api-key']) {
+      if (api_keys && api_keys.indexOf(req.headers['x-api-key']) == -1 && api_keys[api_keys.indexOf(req.headers['x-api-key'])] !== req.headers['x-api-key'] || api_keys.length === 0) {
           res.status(401).send({ error: 'Invalid api_key' });
       } else {
         delete req.headers['x-api-key'];
